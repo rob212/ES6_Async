521541cfabdebf43da3849424b2130bdefb1c6f0
rob212 <robertmcbryde83@gmail.com>
2020-02-29 12:14:42 +0000

    more playing with async and try catch blocks

--- script_ajax_calls_fetch_promise.js
+++ script_ajax_calls_fetch_promise.js
@@ -23,25 +23,46 @@
 
 // Rob async attempt technique. 
 
-async function getEdinburghLocation() {
-    const response =  await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=edinburgh');
-    const data = await response.json();
-    return data[0].woeid;
+// async function getEdinburghLocation() {
+//     const response =  await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=edinburgh');
+//     const data = await response.json();
+//     return data[0].woeid;
+// }
+
+// async function getWeather(code) {
+//     const data = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${code}`);
+//     const json = await data.json();
+//     const location = json.title;
+//     return {data: json, location: location};
+// }
+
+
+// getEdinburghLocation().then(code => {
+//     return getWeather(code);
+// })
+// .then(result => {
+//     const today = result.data.consolidated_weather[0];
+//     const location = result.location;
+//     console.log(`Temperature in ${location} is currently: ${today.the_temp}. The min temp will be ${today.min_temp} and the max with be ${today.max_temp}`);
+// });
+
+
+
+// Same technique again but this time all in one async method and the use of try catch block to handle errors. 
+async function getWeather(woeid) {
+    try {
+        const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
+        const data = await result.json();
+        const tomorrow = data.consolidated_weather[1];
+        console.log(`Temperature in ${data.title} is currently: ${tomorrow.the_temp}. The min temp will be ${tomorrow.min_temp} and the max with be ${tomorrow.max_temp}`);
+        return data;
+    } catch(error) {
+        console.log(`An ERROR occured: ${error}`);
+    }
 }
 
-async function getWeather(code) {
-    const data = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${code}`);
-    const json = await data.json();
-    const location = json.title;
-    return await {data: json, location: location};
-}
-
-
-getEdinburghLocation().then(code => {
-    return getWeather(code);
-})
-.then(result => {
-    const today = result.data.consolidated_weather[0];
-    const location = result.location;
-    console.log(`Temperature in ${location} is currently: ${today.the_temp}. The min temp will be ${today.min_temp} and the max with be ${today.max_temp}`);
+let dataLondon;
+getWeather(44418).then(val => {
+    dataLondon = val;
+    console.log(dataLondon);
 });
